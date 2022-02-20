1346b565c4df3cf2a84573147d78e059f671de29
yuyatinnefeld <yuyawamo@gmail.com>
2022-02-20 10:58:54 +0100

    docs: update clean up part

--- kubernetes/argo-cd/syn.sh
+++ kubernetes/argo-cd/syn.sh
@@ -1,3 +1,7 @@
 # open argocd ui
 
 # change the replicasets / name / another config variabels, push this change and check the ui again
+
+# clean up
+kubectl delete namespace argocd
+kubectl delete namespace yt-app

0c3c66225eb256e6a06772d600f4e981d426ecf9
yuyatinnefeld <yuyawamo@gmail.com>
2022-02-25 19:39:19 +0100

    feat: update wordpress config with cloud sql

--- kubernetes/GKE/step4-wordpress/setup.sh
+++ kubernetes/GKE/step4-wordpress/setup.sh
@@ -1,170 +1,120 @@
-# How to deploy HA wordpress application with GKE
+# Deploying WordPress on GKE with Persistent Disks and Cloud SQL
 
-# about
-# In this lab you will learn how to configure a highly available application by deploying WordPress 
-# using regional persistent disks on Kubernetes Engine. Regional persistent disks provide synchronous replication 
-# between two zones, which keeps your application up and running in case there is an outage or failure 
-# in a single zone. Deploying a Kubernetes Engine Cluster with regional persistent disks will make your 
-# application more stable, secure, and reliable.
+# Objectives
+# - CKE cluster
+# - PV and PVC backed by Persistent Disk
+# - Cloud SWL for MySQL instance
+# - Deploy WordPress
+# - Setup your WordPress blog
 
-# What you'll do
-# - Create a regional Kubernetes Engine cluster.
-# - Create a Kubernetes StorageClass resource that is configured for replicated zones.
-# - Deploy WordPress with a regional disk that uses the StorageClass.
-# - Simulate a zone failure by deleting a node.
-# - Verify that the WordPress app and data migrate successfully to another replicated zone.
 
 # Source
-# https://www.qwiklabs.com/focuses/1050?parent=catalog
+# https://cloud.google.com/kubernetes-engine/docs/tutorials/persistent-disk
 
 
+######### setting up the GKE environment #########
+export PROJECT_ID="yuyatinnefeld-dev"
+export ZONE="europe-west1-b"
+export CLUSTER_NAME="persistent-disk-tutorial"
 
-######### create a regional k8s engine cluster #########
+gcloud config set compute/zone $ZONE
 
-REGION="europe-west1"
-ZONES="europe-west1-b,europe-west1-c,europe-west1-d"
-CLUSTER_VERSION=$(gcloud container get-server-config --region ${REGION} --format='value(validMasterVersions[0])')
-export CLOUDSDK_CONTAINER_USE_V1_API_CLIENT=false
+gcloud services enable container.googleapis.com sqladmin.googleapis.com
 
-# enable kubernetes Engine API
+# clone the repo
+git clone https://github.com/GoogleCloudPlatform/kubernetes-engine-samples
+cd kubernetes-engine-samples/wordpress-persistent-disks
+WORKING_DIR=$(pwd)
 
+# create a GKE cluster
+gcloud container clusters create $CLUSTER_NAME \
+    --num-nodes=3 --enable-autoupgrade --no-enable-basic-auth \
+    --no-issue-client-certificate --enable-ip-alias --metadata \
+    disable-legacy-endpoints=true
 
-# deploy GKE cluster
-gcloud container clusters create repd \
-  --cluster-version=${CLUSTER_VERSION} \
-  --machine-type=n1-standard-4 \
-  --region=${REGION}\
-  --num-nodes=1 \
-  --node-locations=${ZONES}
+# create a pv and a pvc backed by persistent disk
+kubectl apply -f $WORKING_DIR/wordpress-volumeclaim.yaml
+kubectl get persistentvolumeclaim
 
-######### deploying an app with a regional disk #########
+######### create a Cloud SQL for MySQL #########
 
-# add the stable chart repo to helm
-helm repo add stable https://charts.helm.sh/stable
-helm repo update
-helm repo list
+SQL_INSTANCE_NAME=mysql-wordpress-instance
+CLOUD_SQL_PASSWORD=$(openssl rand -base64 18)
 
-# create the storageclass "repd-west1-b-c-d"
+gcloud sql instances create $SQL_INSTANCE_NAME
 
-kubectl apply -f - <<EOF
-kind: StorageClass
-apiVersion: storage.k8s.io/v1
-metadata:
-  name: repd-west1-b-c-d
-provisioner: kubernetes.io/gce-pd
-parameters:
-  type: pd-standard
-  replication-type: regional-pd
-  zones: europe-west1-b,europe-west1-c,europe-west1-d
-EOF
+export INSTANCE_CONNECTION_NAME=$(gcloud sql instances describe $SQL_INSTANCE_NAME --format='value(connectionName)')
 
-# verify
-kubectl get storageclass
+gcloud sql databases create wordpress --instance $SQL_INSTANCE_NAME
 
-######### create persistent volume claims #########
+gcloud sql users create wordpress --host=% --instance $INSTANCE_NAME --password $CLOUD_SQL_PASSWORD
 
-# create data-wp-repd-mariadb-0 PVC with storageclass = standard
+######### secret setup #########
 
-kubectl apply -f - <<EOF
-kind: PersistentVolumeClaim
-apiVersion: v1
-metadata:
-  name: data-wp-repd-mariadb-0
-  namespace: default
-  labels:
-    app: mariadb
-    component: master
-    release: wp-repd
-spec:
-  accessModes:
-    - ReadOnlyMany
-  resources:
-    requests:
-      storage: 8Gi
-  storageClassName: standard
-EOF
+# service account setup
+SA_NAME="cloudsql-proxy"
 
-# create wp-repd-wordpress PVC with storageclass = repd-west1-b-c-d
+gcloud iam service-accounts create $SA_NAME --display-name $SA_NAME
 
-kubectl apply -f - <<EOF
-kind: PersistentVolumeClaim
-apiVersion: v1
-metadata:
-  name: wp-repd-wordpress
-  namespace: default
-  labels:
-    app: wp-repd-wordpress
-    chart: wordpress-5.7.1
-    heritage: Tiller
-    release: wp-repd
-spec:
-  accessModes:
-    - ReadOnlyMany
-  resources:
-    requests:
-      storage: 200Gi
-  storageClassName: repd-west1-b-c-d
-EOF
+SA_EMAIL=$(gcloud iam service-accounts list \
+    --filter=displayName:$SA_NAME \
+    --format='value(email)')
 
-# verify the result
-kubectl get persistentvolumeclaims
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --role roles/cloudsql.client \
+    --member serviceAccount:$SA_EMAIL
 
-######### deploy wordpress #########
+gcloud iam service-accounts keys create $WORKING_DIR/key.json \
+    --iam-account $SA_EMAIL
 
-# deploy wp chart (storageclass = repd-west1-b-c-d)
-helm install wp-repd \
-  --set smtpHost= --set smtpPort= --set smtpUser= \
-  --set smtpPassword= --set smtpUsername= --set smtpProtocol= \
-  --set persistence.storageClass=repd-west1-b-c-d \
-  --set persistence.existingClaim=wp-repd-wordpress \
-  --set persistence.accessMode=ReadOnlyMany \
-  stable/wordpress
+# create secrets for mysql creds
+kubectl create secret generic cloudsql-db-credentials \
+    --from-literal username=wordpress \
+    --from-literal password=$CLOUD_SQL_PASSWORD
 
-# list wordpress pods
-kubectl get pods
-
-# create a service load balancer's external IP
-while [[ -z $SERVICE_IP ]]; do SERVICE_IP=$(kubectl get svc wp-repd-wordpress -o jsonpath='{.status.loadBalancer.ingress[].ip}'); echo "Waiting for service external IP..."; sleep 2; done; echo http://$SERVICE_IP/admin
-
-# verify the persistent disk result
-while [[ -z $PV ]]; do PV=$(kubectl get pvc wp-repd-wordpress -o jsonpath='{.spec.volumeName}'); echo "Waiting for PV..."; sleep 2; done
-kubectl describe pv $PV
-
-# open wordpress URL
-echo http://$SERVICE_IP/admin
-
-# get a username and pwd
-cat - <<EOF
-Username: user
-Password: $(kubectl get secret --namespace default wp-repd-wordpress -o jsonpath="{.data.wordpress-password}" | base64 --decode)
-EOF
-
-# log in with the pwd
-
-######### simulating a zone failure #########
-
-# obtain the current node of the wordpress pod
-NODE=$(kubectl get pods -l app.kubernetes.io/instance=wp-repd  -o jsonpath='{.items..spec.nodeName}')
-ZONE=$(kubectl get node $NODE -o jsonpath="{.metadata.labels['failure-domain\.beta\.kubernetes\.io/zone']}")
-IG=$(gcloud compute instance-groups list --filter="name~gke-repd-default-pool zone:(${ZONE})" --format='value(name)')
-
-# print the result
-echo "Pod is currently on node ${NODE}"
-echo "Instance group to delete: ${IG} for zone: ${ZONE}"
-
-kubectl get pods -l app.kubernetes.io/instance=wp-repd -o wide
-
-# delete instance group for simulation
-gcloud compute instance-groups managed delete ${IG} --zone ${ZONE}
-
-# verify that both the wordpress pod and pv migration to the node that is in the other zone
-kubectl get pods -l app.kubernetes.io/instance=wp-repd -o wide
-
-# open the wordpress admin page
-echo http://$SERVICE_IP/admin
+# create secrets for service account creds
+kubectl create secret generic cloudsql-instance-credentials \
+    --from-file $WORKING_DIR/key.json
 
 
-# clean up
-gcloud container clusters delete repd --region=europe-west1
+######### deploy wordpres #########
 
-helm repo remove stable
+# replacing the INSTANCE_CONNECTION_NAME environment variable
+cat $WORKING_DIR/wordpress_cloudsql.yaml.template | envsubst > $WORKING_DIR/wordpress_cloudsql.yaml
+
+# deploy wordpress sql manifest
+kubectl create -f $WORKING_DIR/wordpress_cloudsql.yaml
+
+kubectl get pod -l app=wordpress --watch
+
+# expose the wordrepss service
+kubectl create -f $WORKING_DIR/wordpress-service.yaml
+kubectl get svc -l app=wordpress --watch
+
+EXTERNAL_IP="35.187.115.206"
+
+curl -i http://$EXTERNAL_IP
+
+# login with the username and password which you previously created
+
+
+######### clean up #########
+
+kubectl delete service wordpress
+
+watch gcloud compute forwarding-rules list
+
+kubectl delete deployment wordpress
+
+kubectl delete pvc wordpress-volumeclaim
+
+gcloud container clusters delete $CLUSTER_NAME
+
+gcloud sql instances delete $INSTANCE_NAME
+
+gcloud projects remove-iam-policy-binding $PROJECT_ID \
+    --role roles/cloudsql.client \
+    --member serviceAccount:$SA_EMAIL
+  
+gcloud iam service-accounts delete $SA_EMAIL
+
